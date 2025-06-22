c4b1efa01042a9bea2c753ed639af175c3013b9f
Soundhiriya M <104375178+riyashinso@users.noreply.github.com>
2025-06-22 09:31:20 +0530

    Add files via upload

---
+++ BusDemo.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class BusDemo {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Bus> buses = new ArrayList<Bus>();
+		ArrayList <Booking> bookings = new ArrayList<Booking>();
+		
+		buses.add(new Bus(1,true,2));
+		buses.add(new Bus(2,true,3));
+		buses.add(new Bus(3,false,1));
+		buses.add(new Bus(4,true,4));
+		buses.add(new Bus(5,true,2));
+		
+		int userOpt = 1;
+		Scanner scanner = new Scanner(System.in);
+		
+		for(Bus b:buses) {
+			b.displayBusInfo();
+		}
+		
+		while(userOpt==1) {
+			System.out.print("Enter 1 to Book and 2 to exit:");
+			userOpt = scanner.nextInt();
+			if(userOpt == 1) {
+				Booking booking = new Booking();
+				
+				if(booking.isAvailable(bookings,buses)) {
+					bookings.add(booking);
+					System.out.println("Your booking is Confirmed.");
+				}
+				else
+					System.out.println("Sorry,Bus is full.Try another bus or date.");
+        scanner.close();            
+		    }
+		}		
+	}
+}
